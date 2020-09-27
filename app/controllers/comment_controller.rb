891fca851c9063397e0740d4a38127e986ae3c10
yujin kimura <yujinkimura@icloud.com>
2020-09-27 16:07:48 +0000

    20200928

---
+++ app/controllers/comment_controller.rb
@@ -0,0 +1,36 @@
+class CommentController < ApplicationController
+  def new
+    if params[:topic_id]
+      @topic = Topic.find_by(id: params[:topic_id]) 
+      @comment = Comment.new
+      @comments = Comment.where(topic_id: params[:topic_id])
+    else
+      @topic = Topic.find_by(id: params[:comment][:topic_id]) 
+      @comment = Comment.new
+      @comments = Comment.where(topic_id: params[:comment][:topic_id])
+    end
+  end
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to comments_new_path(comment: comment_params), success: "コメントを送信しました"
+    else
+      @topic = Topic.find_by(id: params[:comment][:topic_id])
+      @comments = Comment.where(topic_id: params[:comment][:topic_id])
+      flash.now[:danger] = "コメントを送れません"
+      render :new
+    end
+  end
+    
+  def destroy
+    comment =Comment.find_by(id: params[:comment_id])
+    comment.destroy
+    redirect_to topics_path, info: "コメントを削除しました"
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:sentence, :topic_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,24 @@
-
 Rails.application.routes.draw do
+  get 'topics/new'
   get 'sessions/new'
-
   root 'pages#index'
   get 'pages/help'
-
+  
+  resources :users
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-
- 
+  
   resources :users
   resources :topics
+  
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
+  delete '/favorites', to:"favorites#destroy"
   
+  get 'comments/new' 
+  post '/comments', to: "comments#create"
+  delete '/comments', to: "comments#destroy"
+
 end
